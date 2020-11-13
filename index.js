8416527637e01edea5cf6262ed9f7b46296f0f82
Harvy <harvyestebanml@gmail.com>
2020-11-13 16:17:40 -0500

    added PORT to index.js

--- index.js
+++ index.js
@@ -120,4 +120,4 @@ function compareValues(key, order = 'asc') {
 }
 
 
-app.listen(3000, () => console.log("listening on port 3000"))
+app.listen(process.env.PORT || 3000, () => console.log("listening on port 3000"))
